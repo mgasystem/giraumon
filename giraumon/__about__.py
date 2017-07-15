d417cafc7e7586f0e7b27ef9c1a3738a27592028
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-07-15 15:05:35 +0200

    Feature/core (#2)
    Add subcommand init, to create the .platform and manifest.json
    
    * Add structure for giraumon
    * Add requirement for installation
    * Split command in multiple file
    * Add test for cli
    * Add option to force the creation of git repo
    * Add function to create folder an manifest
    * Improve test on initialize
    * Add test with force option
    * Add yaml file for codecov
    * Add test when create manifest with no path
    * Add Giraumon infromation for packaging
    * Add new test to increase cover
    * Add some badges

---
+++ giraumon/__about__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl).
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "giraumon"
+__summary__ = "Core utilities for Python packages"
+__uri__ = "https://github.com/mgasystem/giraumon"
+
+__version__ = "0.1.0"
+
+__author__ = "Christophe cHAUVET"
+__email__ = "christophe.chauvet@gmail.com"
+
+__license__ = "GPLv3"
+__copyright__ = "Copyright 2016-2017 %s" % __author__
