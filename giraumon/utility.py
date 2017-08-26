53776a4ae112123f47c0c9f39bc280503871439b
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-08-26 18:43:03 +0200

    Add new function to manage repo, manifest and folder

--- giraumon/utility.py
+++ giraumon/utility.py
@@ -2,8 +2,12 @@
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl).
 
 import os
+import shutil
 import json
+from git import Repo
+from git.exc import InvalidGitRepositoryError
 from .__about__ import __version__
+from giraumon import logger
 
 MANIFEST_FILE = 'manifest.json'
 
@@ -29,11 +33,13 @@ def create_manifest(path=''):
     if not path:
         return False
 
-    mf = os.path.join(path, MANIFEST_FILE)
+    pfdir = os.path.join(path, '.platform')
+    if not os.path.lexists(pfdir):
+        os.mkdir(pfdir)
 
+    mf = os.path.join(path, '.platform', MANIFEST_FILE)
     if not os.path.lexists(mf):
-        with open(mf, 'w') as fd:
-            fd.write(json.dumps(MANIFEST, indent=2))
+        write_manifest(path, MANIFEST)
 
     return True
 
@@ -51,3 +57,65 @@ def create_folder(path='', gitkeep=False):
             fd.write('# This is unused, only for git compatibility')
 
     return True
+
+
+def find_git_root():
+    """
+    Detect if the current directory is in a git tree
+    Return the root git directory
+    """
+    try:
+        repo = Repo(os.getcwd())
+        return repo.working_dir
+    except InvalidGitRepositoryError:
+        logger.error('%s Not a valid repository' % os.getcwd())
+        return ''
+
+
+def check_manifest(path):
+    """
+    Check if the current repository have a manifest.json
+    in the .platform folder
+    """
+    pfdir = os.path.join(path, '.platform', MANIFEST_FILE)
+    return os.path.exists(pfdir)
+
+
+def read_manifest(path):
+    """
+    Read the manifest and return a dict
+    """
+    mf = os.path.join(path, '.platform', MANIFEST_FILE)
+    return json.load(open(mf, 'r'))
+
+
+def write_manifest(path, content):
+    """
+    Write the manifest content to the manifest.json
+    """
+    mf = os.path.join(path, '.platform', MANIFEST_FILE)
+    with open(mf, 'w') as fd:
+        fd.write(json.dumps(content, indent=2, sort_keys=True))
+
+
+def add_template(path, filename):
+    """
+    Save the template in the .platform/templates
+    directory and add extension j2 for Jinja2
+    """
+    # Retrieve only the filename of the original file
+    dest_filename = filename.split(os.sep)[-1]
+    tpl = os.path.join(path, '.platform', 'templates', dest_filename + '.j2')
+    shutil.copyfile(filename, tpl)
+    mf_data = read_manifest(path)
+    # Check if this template already in a manifest
+    for d in mf_data.get('templates', []):
+        if dest_filename in d.get('source', ''):
+            break
+    else:
+        mf_data['templates'].append({
+            'source': dest_filename,
+            'destination': '',
+            'chmod': '600',
+        })
+        write_manifest(path, mf_data)
