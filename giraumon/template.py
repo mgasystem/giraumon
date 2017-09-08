7c6807065a9dfcbb8d921b1c5f7d7a94deaa9e17
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-09-08 17:48:56 +0200

    Add option to render a template

--- giraumon/template.py
+++ giraumon/template.py
@@ -12,10 +12,13 @@ from .utility import find_git_root, check_manifest, add_template
 @click.command()
 @click.option('-a', '--add', type=click.Path(
     exists=True, file_okay=True, readable=True, resolve_path=True))
+@click.option('-r', '--render', type=click.Path(
+    exists=True, file_okay=True, readable=True, resolve_path=True))
 @click.argument('path', type=click.Path())
-def template(add, path):
+def template(add, render, path):
     """
-    Copy template to the folder template
+    Add option: Copy template to the folder template
+    Render: take template and render it
     """
 
     wk_dir = find_git_root(path)
